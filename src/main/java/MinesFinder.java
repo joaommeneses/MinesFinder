99ced88c1585ced7962038bf6e3d6614dacc899b
João Santos <joaomeneses1804@gmail.com>
2022-03-09 15:00:35 +0000

    Definicao do conteudo da janela

--- src/main/java/MinesFinder.java
+++ src/main/java/MinesFinder.java
@@ -1,12 +1,35 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class MinesFinder extends JFrame {
     private JPanel painelPrincipal;
+    private JLabel JLabelNorth;
+    private JButton sairButton;
+    private JButton jogoMédioButton;
+    private JButton jogoFácilButton;
+    private JButton jogoDíficilButton;
+    private JButton nomeJogadorButton2;
+    private JButton nívelDíficil9999Button;
+    private JButton nomeJogadorButton1;
+    private JButton nívelMédio9999Button;
+    private JButton nomeJogadorButton;
+    private JButton nívelFácil9999Button;
+    private JButton recordesButton;
+
     public MinesFinder(String title) {
         super(title);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setContentPane(painelPrincipal);
         pack();
+        //Código criado ao fazer botao direito no botao sair e create listener action listener
+        sairButton.addActionListener(this::btnSairActionPerformed);
     }
+
+    private void btnSairActionPerformed(ActionEvent e) {
+        System.exit(0);
+    }
+
     public static void main(String[] args) {
         new MinesFinder("Mines Finder").setVisible(true);
     }
